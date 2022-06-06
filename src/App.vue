8993e4301693198bb66712314d55163143beb71d
gavan <guoming3@xiaomi.com>
2022-06-06 15:56:28 +0800

    prepare route.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import SelectDemo from './components/SelectDemo.vue'
-import SelectDemo2 from './components/SelectDemo2.vue'
-import SelectDemo3 from './components/SelectDemo3.vue'
-import ServerCaseDemo from './components/ServerCaseDemo.vue'
-import server11 from './components/data/server-1-service-1-ip.json'
-import server12 from './components/data/server-1-service-1-ip.json'
-</script>
-
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
-  <!-- <SelectDemo />
+  <SelectDemo />
   <SelectDemo2 />
-  <SelectDemo3 /> -->
-  <div>
-    <!-- <ServerCaseDemo /> -->
-  </div>
+  <SelectDemo3 /> 
   <div>
     <ServerCaseDemo log-name="server11" :data="server11" />
     <ServerCaseDemo log-name="server12" :data="server12" />
   </div>
 </template>
 
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import SelectDemo from './components/SelectDemo.vue'
+import SelectDemo2 from './components/SelectDemo2.vue'
+import SelectDemo3 from './components/SelectDemo3.vue'
+import ServerCaseDemo from './components/ServerCaseDemo.vue'
+import server11 from './data/server-1-service-1-ip.json'
+import server12 from './data/server-1-service-1-ip.json'
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import App from './App.vue'
+import routes from './routes'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Entry from './Entry.vue'
 
-const app = createApp(App)
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+const app = createApp(Entry)
 app.use(ElementPlus)
+app.use(router)
 app.mount('#app')
